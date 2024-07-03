47e7525c7064c857e6d0e9cc360d8f97a360e129
yasen <ypeychev@internal.oculust-tech.com>
2024-07-03 17:42:24 +0400

    added filters based on fft size

--- src/cxs_analyzer.py
+++ src/cxs_analyzer.py
@@ -38,7 +38,7 @@ class SdrSig():
         Generates frequency, even if it falls in the 0th fft bin/position.
         :return: list of absolute frequencies
         """
-        frequency_step = 1e6  # Hz
+        frequency_step = 35e6  # Hz
         start_frequency = self.center_freq - self.sample_rate / 2
         end_frequency = self.center_freq + self.sample_rate / 2
         sr = int(np.ceil(start_frequency / frequency_step) * frequency_step)  # rounded value
@@ -52,7 +52,7 @@ class SdrSig():
             freq_list.append(f_cur)
         return freq_list
 
-    def read_samples(self, size):
+    def travers_samples(self, size):
         '''
         Actual rtlsdr returns iq data in complex notation.
         This currently returns real data
@@ -60,29 +60,44 @@ class SdrSig():
         gainMult = 10 ** (self.gain / 10)
         sample_duration = size / self.sample_rate
         t_start = random.random()
-        t_times = np.linspace(t_start, int(t_start + sample_duration), int(self.sample_rate * sample_duration))
+        self.t_times = np.linspace(t_start, int(t_start + sample_duration), int(self.sample_rate * sample_duration))
         signal_time_domain = []
-        f = np.array(self.frequency_generator_relative())
+        # f = np.array(self.frequency_generator_relative())
         f = np.array(self.frequency_generator_absolute())
         print(
             "INFO:testfft_rtlsdr: freqs [{}], sampRate [{}], tStart [{}], dur [{}], len [{}]".format(
-                self.center_freq - f, self.sample_rate, t_start, sample_duration, len(t_times)))
-        s = np.zeros(len(t_times), dtype=complex)
+                self.center_freq - f, self.sample_rate, t_start, sample_duration, len(self.t_times)))
+        s = np.zeros(len(self.t_times), dtype=complex)
         for i in range(len(f)):
-            sS = gainMult * np.sin(2 * np.pi * f[i] * t_times)
-            sC = gainMult * np.cos(2 * np.pi * f[i] * t_times)
+            sS = gainMult * np.sin(2 * np.pi * f[i] * self.t_times)
+            sC = gainMult * np.cos(2 * np.pi * f[i] * self.t_times)
             signal_time_domain.append(sS + sC * 1j)
             s += signal_time_domain[i]
         return s
 
 
-
     def test(self):
-        data_buffer = self.read_samples(self.sample_rate)
+        data_buffer = self.travers_samples(random.randint(1,10)*2**16)
         freqs = np.linspace(0, len(data_buffer) * 1/self.sample_rate * int(self.sample_rate / self.center_freq))
+        print(freqs)
+        win = np.kaiser(len(data_buffer), 15)
+        win = np.hanning(len(data_buffer))
+        freqs = np.fft.fftshift(np.fft.fftfreq(len(data_buffer), 1 / self.sample_rate)) + self.center_freq
+        print("INFO:testfft_rtlsdr: freqs [{}] - [{}], fftSize [{}]".format(min(freqs), max(freqs), len(data_buffer)))
+        fftCur = np.abs(np.fft.fft(data_buffer)) / len(data_buffer)
+        fftCur = np.fft.fftshift(fftCur)
+        fftCurWin = np.abs(np.fft.fft(data_buffer * win)) / len(data_buffer)
+        fftCurWin = np.fft.fftshift(fftCurWin)
         plt.subplot(2, 2, 1)
-        plt.plot(freqs, data_buffer)
+        plt.plot(freqs, fftCur)
+        plt.subplot(2, 2, 2)
+        plt.plot(freqs, 10 * np.log10(fftCur))
+        plt.subplot(2, 2, 3)
+        plt.plot(freqs, fftCurWin)
+        plt.subplot(2, 2, 4)
+        plt.plot(freqs, 10 * np.log10(fftCurWin))
         plt.show()
+
 
 
 
